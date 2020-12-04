7cece26a8bddecf6061f6c4374c2279517b15f75
GallantFusion <66767315+GallantFusion@users.noreply.github.com>
2020-12-05 00:30:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -154,8 +154,8 @@ app.post('/RHeader',function(req, res){
 		request.input('input_parameter4', sql.Int, imageID)
 		request.input('input_parameters5', sql.Int, DriverID)
 		request.input('input_parameters6', sql.Int, DriverS)
-		request.input('input_parameters7', sql.DateTime, DC)
-		request.input('input_parameters8', sql.NvarChar, CC)
+		request.input('input_parameters7', sql.DateTime, DateCollected)
+		request.input('input_parameters8', sql.NvarChar, CustomerContact)
 		request.output('ouput_parameter', sql.Int)
 		request.query(`INSERT INTO dbo.RET_ReturnsHeader (CustomerID , DateTime, Status, LoginID, ReturnImageID, DriverID, DriverSigned, DateCollected) OUTPUT Inserted.ID VALUES ( @input_parameters, @input_parameters1, @input_parameters3, @input_parameters2, @input_parameters4, @input_parameters5, @input_parameters6, @input_parameters7)`, function(err, result){
 			if(err) console.log(err)
